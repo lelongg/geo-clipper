2135145789182b817c758017d8eec91291e167a5
Gérald Lelong <gerald.lelong@easymov.fr>
2019-08-19 11:55:16 +0200

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,51 @@
+//! This crates allows to perform boolean operations on polygons.
+//!
+//! It make use of [clipper-sys](https://github.com/lelongg/clipper-sys) which is a binding to the C++ version of [Clipper](http://www.angusj.com/delphi/clipper.php).
+//!
+//! # Example
+//!
+//! The following example shows how to compute the intersection of two polygons.  
+//! The [`intersection`] method (as well as [`difference`], [`union`] and [`xor`]) is provided by the [`Clipper`] trait which is implemented for some [geo-types](https://docs.rs/geo-types/0.4.3/geo_types/).
+//!
+//! ```
+//! # fn main() {
+//! use geo_types::{Coordinate, LineString, Polygon};
+//! use geo_clipper::Clipper;
+//!
+//! let subject = Polygon::new(
+//!     LineString(vec![
+//!         Coordinate { x: 180.0, y: 200.0 },
+//!         Coordinate { x: 260.0, y: 200.0 },
+//!         Coordinate { x: 260.0, y: 150.0 },
+//!         Coordinate { x: 180.0, y: 150.0 },
+//!     ]),
+//!     vec![LineString(vec![
+//!         Coordinate { x: 215.0, y: 160.0 },
+//!         Coordinate { x: 230.0, y: 190.0 },
+//!         Coordinate { x: 200.0, y: 190.0 },
+//!     ])],
+//! );
+//!
+//! let clip = Polygon::new(
+//!     LineString(vec![
+//!         Coordinate { x: 190.0, y: 210.0 },
+//!         Coordinate { x: 240.0, y: 210.0 },
+//!         Coordinate { x: 240.0, y: 130.0 },
+//!         Coordinate { x: 190.0, y: 130.0 },
+//!     ]),
+//!     vec![],
+//! );
+//!
+//! let result = subject.intersection(&clip, 1.0);
+//! # }
+//! ```
+//!
+//! [`Clipper`]: trait.Clipper.html
+//! [`intersection`]: trait.Clipper.html#method.intersection
+//! [`difference`]: trait.Clipper.html#method.difference
+//! [`union`]: trait.Clipper.html#method.union
+//! [`xor`]: trait.Clipper.html#method.xor
+
 use clipper_sys::{
     execute, free_polygons, ClipType, ClipType_ctDifference, ClipType_ctIntersection,
     ClipType_ctUnion, ClipType_ctXor, Path, PolyFillType_pftNonZero, PolyType, PolyType_ptClip,
@@ -190,6 +238,10 @@ fn execute_boolean_operation<T: ToOwnedPolygon + ?Sized, U: ToOwnedPolygon + ?Si
     result
 }
 
+/// This trait defines the boolean operations between polygons.
+///
+/// The `factor` parameter in its methods is used to scale shapes before and after applying the boolean operation
+/// to avoid precision loss since Clipper (the underlaying library) performs integer computation.
 pub trait Clipper<T: ?Sized> {
     fn difference(&self, other: &T, factor: f64) -> MultiPolygon<f64>;
     fn intersection(&self, other: &T, factor: f64) -> MultiPolygon<f64>;
