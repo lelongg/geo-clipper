5aac4923c7ece98adac77c8c5efdc2b13f2d499b
Gérald Lelong <gerald.lelong@easymov.fr>
2020-09-16 08:57:48 +0200

    sync readme & change default.nix to shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  mozilla = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ mozilla ]; };
+in with pkgs;
+let
+  rust = (rustChannelOf {
+    channel = "stable";
+    date = "2020-08-03";
+  }).rust.override { extensions = [ "rust-src" ]; };
+in mkShell {
+  buildInputs = [ clang rust openssl pkgconfig cmake ];
+  LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+}
